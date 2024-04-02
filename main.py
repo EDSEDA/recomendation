b056f505431c0fb2701671739595bf62cb3ff8e6
Vorkov Nikita <vorkov711@gmail.com>
2024-04-02 22:46:44 +0300

    correct parsing

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+import json
 from logging import info, error
 import pickle
 import asyncio
 
+from grifon.config import settings
 from grifon.mqbroker.kafka_client import KafkaClient
 from grifon.recommendation.schema import CreateUserRecommendationMessage, UserRecommendationMessage
 
 from config import settings
 from inference.model import Model
-
-# !!!!!! change me !!!!!!
-topic_name = "my_topic"
 
 kafka_client = KafkaClient("localhost:9092")
 
@@ -18,9 +17,10 @@ with open(f'./recommendation/{settings.version}.pickle', 'rb') as file:
     model: Model = pickle.load(file)
 
 
-@kafka_client.register_topic_handler(topic_name)
+@kafka_client.register_topic_handler(settings.RECOMMENDATION_REQUEST_TOPIC)
 def handle_create_user_recommendation_message(create_message: CreateUserRecommendationMessage):
     try:
+        create_message = CreateUserRecommendationMessage.parse_obj(json.loads(create_message.value()))
         info(f'Creating recommendations: {create_message}')
 
         recommendations = model.get_recommendations() # пока без параметров
@@ -30,7 +30,7 @@ def handle_create_user_recommendation_message(create_message: CreateUserRecommen
             cash_register_id=create_message.cash_register_id,
             recommendations=recommendations
         )
-        kafka_client.send_message(topic_name, user_recommendation_message)
+        kafka_client.send_message(settings.RECOMMENDATION_RESPONSE_TOPIC, user_recommendation_message)
         kafka_client.flush()
 
         info(f'Recommendations generated: {create_message}')
