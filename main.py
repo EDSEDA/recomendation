d609e3e0edb667b0ca6b6f572fcc161b338974ed
NikitaSulimenko <nikkitkit0707@mail.ru>
2024-04-07 19:36:15 +0300

    add deserialize_msg when msg received

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import json
-from logging import info, error
-import pickle
 import asyncio
+import pickle
+from logging import info
 
+from config import settings
 from grifon.config import settings
 from grifon.mqbroker.kafka_client import KafkaClient
 from grifon.recommendation.schema import CreateUserRecommendationMessage, UserRecommendationMessage
-
-from config import settings
 from inference.model import Model
 
 kafka_client = KafkaClient("localhost:9092")
@@ -17,25 +15,21 @@ with open(f'./recommendation/{settings.version}.pickle', 'rb') as file:
     model: Model = pickle.load(file)
 
 
-@kafka_client.register_topic_handler(settings.RECOMMENDATION_REQUEST_TOPIC)
+@kafka_client.register_topic_handler(settings.RECOMMENDATION_REQUEST_TOPIC, msg_class=CreateUserRecommendationMessage)
 async def handle_create_user_recommendation_message(create_message: CreateUserRecommendationMessage):
-    try:
-        create_message = CreateUserRecommendationMessage.parse_obj(json.loads(create_message.value()))
-        info(f'Creating recommendations: {create_message}')
+    info(f'Creating recommendations: {create_message}')
 
-        recommendations = model.get_recommendations() # пока без параметров
+    recommendations = model.get_recommendations()   # пока без параметров
 
-        user_recommendation_message = UserRecommendationMessage(
-            user_id=create_message.user_id,
-            cash_register_id=create_message.cash_register_id,
-            recommendations=recommendations
-        )
-        kafka_client.send_message(settings.RECOMMENDATION_RESPONSE_TOPIC, user_recommendation_message)
-        kafka_client.flush()
+    user_recommendation_message = UserRecommendationMessage(
+        user_id=create_message.user_id,
+        cash_register_id=create_message.cash_register_id,
+        recommendations=recommendations
+    )
+    kafka_client.send_message(settings.RECOMMENDATION_RESPONSE_TOPIC, user_recommendation_message)
+    kafka_client.flush()
 
-        info(f'Recommendations generated: {create_message}')
-    except Exception as e:
-        error(e)
+    info(f'Recommendations generated: {create_message}')
 
 
 async def main():
